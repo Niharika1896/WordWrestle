9b9cf4ea6e90f5c698351d244da38cc2fd64381f
Niharika Sinha <niharika.sinha1896@gmail.com>
2022-11-28 00:10:49 -0500

    Generalizing game for n players

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def getNewServerWord(server_word, new_word):
     find_start_index = len(server_word) - len(new_word)
     for i in range(len(new_word)):
         substrng = new_word[0:len(new_word) - i]
-        server_idx = server_word.find(substrng, find_start_index+i)
+        server_idx = server_word.find(substrng, find_start_index + i)
         if server_idx == -1 or server_idx + len(substrng) < len(server_word):
             if i == len(new_word) - 1:
                 new_server_word = server_word + new_word
@@ -55,8 +55,13 @@ def mirrorWord(word):
 
 
 def run(num_rounds, num_players):
-    player1 = Player()
-    player2 = Player()
+    players = dict()
+    scores = dict()
+    for i in range(1, num_players + 1):
+        p = Player()
+        pid = 'player' + str(i)
+        players[pid] = p
+        scores[pid] = 0
 
     game_words_played_set = set()
     game_words_played_list = []
@@ -77,79 +82,65 @@ def run(num_rounds, num_players):
     current_round = 1
     current_server_word = starting_word
 
-    player1_score = 0
-    player2_score = 0
-
     print('SERVER WORD:   ', starting_word)
     while current_round <= total_rounds:
         print('\n____________________Round ', current_round, '____________________')
-        # Client 1 plays first
-        player1_powers_used, player1_words_played = player1.play(current_server_word, game_words_played_set,
-                                                                 total_rounds - current_round + 1, total_rounds, 1)
 
-        # Checks
-        while len(player1_powers_used) == 0 or len(player1_words_played) == 0 or len(player1_powers_used) != len(
-                player1_words_played) or (len(player1_powers_used) == 1 and player1_powers_used[0] == 'gotwice'):
-            player1_powers_used, player1_words_played = player1.play(current_server_word, game_words_played_set,
-                                                                     total_rounds - current_round + 1, total_rounds,
-                                                                     1)
+        for i in range(1, num_players + 1):
+            pid = 'player' + str(i)
+            player = players[pid]
+            player_powers_used, player_words_played = player.play(current_server_word, game_words_played_set,
+                                                                  total_rounds - current_round + 1, total_rounds, i)
+            # Checks
+            while len(player_powers_used) == 0 or len(player_words_played) == 0 or len(player_powers_used) != len(
+                    player_words_played) or (len(player_powers_used) == 1 and player_powers_used[0] == 'gotwice'):
+                player_powers_used, player_words_played = player.play(current_server_word, game_words_played_set,
+                                                                      total_rounds - current_round + 1, total_rounds, i)
 
-        for index, value in enumerate(player1_powers_used):
-            if value == 'flip':
-                temp_server_word = current_server_word[::-1]
-            elif value == 'mirror':
-                temp_server_word = mirrorWord(current_server_word)
-            elif value == 'remove':
-                temp_server_word = current_server_word[0:len(current_server_word) - 1]
-            elif value == 'gotwice' or value == 'nopower':
-                temp_server_word = current_server_word
-            current_server_word, sc = getNewServerWord(temp_server_word, player1_words_played[index])
-            player1_score += sc
-            print('Player1: Power Used - ', value, '  | Word Played - ', player1_words_played[index],
-                  '  | Points Gained - ', sc, '  | Player1 Score - ', player1_score)
-            print('SERVER WORD:   ', current_server_word)
-            game_words_played_set.add(player1_words_played[index])
-            game_words_played_list.append(player1_words_played[index])
-
-        # Client 2 plays second
-        player2_powers_used, player2_words_played = player2.play(current_server_word, game_words_played_set,
-                                                                 total_rounds - current_round + 1, total_rounds, 2)
-
-        # Checks
-        while len(player2_powers_used) == 0 or len(player2_words_played) == 0 or len(player2_powers_used) != len(
-                player2_words_played) or (len(player2_powers_used) == 1 and player2_powers_used[0] == 'gotwice'):
-            client2_powers_used, player2_words_played = player2.play(current_server_word, game_words_played_set,
-                                                                     total_rounds - current_round + 1, total_rounds,
-                                                                     2)
-
-        for index, value in enumerate(player2_powers_used):
-            if value == 'flip':
-                temp_server_word = current_server_word[::-1]
-            elif value == 'mirror':
-                temp_server_word = mirrorWord(current_server_word)
-            elif value == 'remove':
-                temp_server_word = current_server_word[0:len(current_server_word) - 1]
-            elif value == 'gotwice' or value == 'nopower':
-                temp_server_word = current_server_word
-            current_server_word, sc = getNewServerWord(temp_server_word, player2_words_played[index])
-            player2_score += sc
-            print('Player2: Power Used - ', value, '  | Word Played - ', player2_words_played[index],
-                  '  | Points Gained - ', sc, '  | Player2 Score - ', player2_score)
-            print('SERVER WORD:   ', current_server_word)
-            game_words_played_set.add(player2_words_played[index])
-            game_words_played_list.append(player2_words_played[index])
+            for index, value in enumerate(player_powers_used):
+                if value == 'flip':
+                    temp_server_word = current_server_word[::-1]
+                elif value == 'mirror':
+                    temp_server_word = mirrorWord(current_server_word)
+                elif value == 'remove':
+                    temp_server_word = current_server_word[0:len(current_server_word) - 1]
+                elif value == 'gotwice' or value == 'nopower':
+                    temp_server_word = current_server_word
+                current_server_word, sc = getNewServerWord(temp_server_word, player_words_played[index])
+                scores[pid] += sc
+                print(pid, ': Power Used - ', value, '  | Word Played - ', player_words_played[index],
+                      '  | Points Gained - ', sc, '  | Player1 Score - ', scores[pid])
+                print('SERVER WORD:   ', current_server_word)
+                game_words_played_set.add(player_words_played[index])
+                game_words_played_list.append(player_words_played[index])
 
         current_round += 1
 
-    if player1_score > player2_score:
-        print('Client 1 wins!')
-    elif player1_score < player2_score:
-        print('Client 2 wins!')
+        printResults(scores)
+
+
+def printResults(scores):
+    max_score = 0
+    winners = []
+    print('\n --------------- Final Scores: ---------------')
+    for pid in scores:
+        print(pid, ' -> ', scores[pid])
+        if scores[pid] > max_score:
+            max_score = scores[pid]
+
+    for pid in scores:
+        if scores[pid] == max_score:
+            winners.append(pid)
+
+    if len(winners) == 1:
+        print(winners[0], ' Wins !!! ')
+    elif len(winners) > 1:
+        print('It is a tie between ', winners)
     else:
-        print('Its a tie!')
+        print('Something went wrong :( ')
 
 
 if __name__ == '__main__':
     num_rounds = 7
-    num_players = 2
+    num_players = 5
     run(num_rounds, num_players)
